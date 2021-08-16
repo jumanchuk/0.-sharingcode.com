3edb78f7865b63b8c7f22ff1ab292142071a94ee
jumanchuk <juryumanchuk@gmail.com>
2021-08-16 16:34:43 -0300

    New Codebox

--- js/main.js
+++ js/main.js
@@ -1,17 +1,41 @@
+function showCodeBox(){
+
+    let element = document.getElementById("code-box");
+    
+    if(element.hidden == true){
+        element.hidden = false;
+    }else{
+        element.value ="";
+        element.hidden = true;
+    }
+}
+
 function send(){
 
-    let text = document.getElementById('crd-text').value;
-    const card = new Card("Jury Umanchuk", "@jumanchuk", text);
+    let text = document.getElementById('crd-text').textContent;
+    let code = document.getElementById('code-box').innerText;
+
+    const card = new Card("Jury Umanchuk", "@jumanchuk", text,code);
+
+    clearBoxes();
     
     card.print();
 }
 
+function clearBoxes(){
+
+    document.getElementById('crd-text').innerHTML = "";
+    document.getElementById('code-box').innerHTML = "";
+}
+
+
 //Object Card
-function Card(name,user,text){
+function Card(name,user,text,code){
 
     this.name = name;
     this.user   = user;
     this.text  = text;
+    this.code  = code;
 
     let error = document.getElementById('error');
 
@@ -29,7 +53,8 @@ function Card(name,user,text){
             document.getElementById("crd-text").value = ""
             document.getElementById('crd-principal').insertAdjacentHTML('afterend',
             '<div class="col-12 card-border"><div class="container"><div class="row"><div class="col-2"><img src="img/user.png" ' +
-            'class="rounded-circle user-img" alt="Cinque Terre"></div><div class="col"><div class="crd-label userName">' + this.name + ' <span class="userId">'+ this.user +'k</span></div><div class="crd-label">'+ this.text +'</div>' +
+            'class="rounded-circle user-img" alt="Cinque Terre"></div><div class="col-10"><div class="crd-label userName">' + this.name + '<span class="userId">'+
+             this.user +'</span></div><div class="crd-label">'+ this.text +'<div class="codebox"><pre><code data-language="css">'+ this.code +'</code></pre></div>' +
             '<div class="d-flex justify-content-around card-submenu"><a class="p-2 fa-tw-icons rounded-circle" href="#">'+
             '<i class="fa fa-comment-o"> 0</i></a><a class="p-2 fa-tw-icons rounded-circle" href="#" onclick="bookmark(this)"><i class="fa fa-star"></i></a>'+
             '<a class="p-2 fa-tw-icons rounded-circle" href="#" onclick="rtw(this)"><i class="fa fa-retweet"> 0</i>'+
@@ -41,9 +66,11 @@ function Card(name,user,text){
 }
 
 function like(element){
-    
+
+        let value = element.children[0].textContent;    
+
     if(!element.classList.contains("like")) {
-        let value = element.children[0].textContent;
+
         let sum =  parseInt(value) + parseInt(1);
 
         if(sum<1){
@@ -56,13 +83,12 @@ function like(element){
         element.classList.add("like");
     } else{
 
-        let value = element.children[0].textContent;
         let sum =  parseInt(value) - parseInt(1);
 
         if(sum<1){
         element.children[0].textContent = ' 0';
         }else{
-        element.children[0].textContent = '\u00A0' - sum;
+        element.children[0].textContent = '\u00A0' + sum;
         }
 
         element.style.setProperty("color", "white", "important");
@@ -72,9 +98,11 @@ function like(element){
 
 
 function rtw(element){
-    
-    if(!element.classList.contains("rtw")) {
+
         let value = element.children[0].textContent;
+
+    if(!element.classList.contains("rtw")) {
+
         let sum =  parseInt(value) + parseInt(1);
 
         if(sum<1){
@@ -87,13 +115,12 @@ function rtw(element){
         element.classList.add("rtw");
     } else {
 
-        let value = element.children[0].textContent;
         let sum =  parseInt(value) - parseInt(1);
 
         if(sum<1){
             element.children[0].textContent = ' 0';
         }else{
-        element.children[0].textContent = '\u00A0' - sum;
+        element.children[0].textContent = '\u00A0' + sum;
         }
         
         element.style.setProperty("color", "white", "important");
@@ -103,16 +130,35 @@ function rtw(element){
 }
 
 function bookmark(element){
+
+    
+    let value = document.getElementById("fav").children[0].textContent;
     
     if(!element.classList.contains("bookmark")) {
 
         element.style.setProperty("color", "rgb(247, 252, 0)", "important");
         element.classList.add("bookmark");
+
+        let sum =  parseInt(value) + parseInt(1);
+
+        if(sum<1){
+            document.getElementById("fav").children[0].textContent= ' 0';
+        }else{
+            document.getElementById("fav").children[0].textContent = '\u00A0' + sum;
+        }
                                 
     } else {
 
         element.style.setProperty("color", "white", "important");
         element.classList.remove("bookmark");
+        
+        let sum =  parseInt(value) - parseInt(1);
+
+        if(sum<1){
+            document.getElementById("fav").children[0].textContent= ' 0';
+        }else{
+            document.getElementById("fav").children[0].textContent = '\u00A0' + sum;
+        }
 
     }
 }
