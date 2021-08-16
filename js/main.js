90a61c9d57432873871a47af70e7e76ebb552927
jumanchuk <juryumanchuk@gmail.com>
2021-08-16 19:43:23 -0300

    CodeBox Optimization for Html

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function send(){
     const card = new Card("Jury Umanchuk", "@jumanchuk", text,code);
 
     clearBoxes();
-    
+
     let element = document.getElementById("code-box");
     element.hidden = true;
 
@@ -62,7 +62,7 @@ function Card(name,user,text,code){
             document.getElementById('crd-principal').insertAdjacentHTML('afterend',
             '<div class="col-12 card-timeline"><div class="container"><div class="row"><div class="col-2"><img src="img/user.png" ' +
             'class="rounded-circle user-img" alt="Cinque Terre"></div><div class="col-10"><div class="crd-label userName">' + this.name + '<span class="userId">'+
-             this.user +'</span></div><div class="crd-label">'+ this.text +'<div class="codebox" '+ hidden +'><pre><code data-language="css">'+ this.code +'</code></pre></div>' +
+             this.user +'</span></div><div class="crd-label">'+ this.text +'<div class="codebox" '+ hidden +'><pre><code data-language="css"><xmp>'+ this.code +'</xmp></code></pre></div>' +
             '<div class="d-flex justify-content-around card-submenu"><a class="p-2 fa-tw-icons rounded-circle" href="#">'+
             '<i class="fa fa-comment-o"> 0</i></a><a class="p-2 fa-tw-icons rounded-circle" href="#" onclick="bookmark(this)"><i class="fa fa-star"></i></a>'+
             '<a class="p-2 fa-tw-icons rounded-circle" href="#" onclick="rtw(this)"><i class="fa fa-retweet"> 0</i>'+
