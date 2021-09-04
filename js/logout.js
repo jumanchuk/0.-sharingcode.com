039240a76cd51dddb3088aad91f9b852e446b4fb
jumanchuk <juryumanchuk@gmail.com>
2021-09-04 01:28:52 -0300

    Fix Logout Event for md & sm

--- js/logout.js
+++ js/logout.js
@@ -1,6 +1,11 @@
 
 let buttom = document.getElementById("btn-logout");
+let buttomMd = document.getElementById("btn-md-logout");
+let buttomSm = document.getElementById("btn-sm-logout");
+
 buttom.addEventListener("click",LogOut);
+buttomMd.addEventListener("click",LogOut);
+buttomSm.addEventListener("click",LogOut);
 
 function LogOut(){
 
